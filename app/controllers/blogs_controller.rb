6fd2790bc880fe5a84be35fda4976a87677c8c0b
ryo-ima <i.riino5y@gmail.com>
2019-09-10 00:18:32 +0900

    create_destroy_action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,11 @@ class BlogsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    @blogs = Blog.all
+  end
+
+  def show
+    redirect_to action: "index"
   end
 
   def new
@@ -9,18 +14,30 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
+    Blog.create(title: blog_params[:title],body: blog_params[:body],image: blog_params[:image],user_id: current_user.id)
     redirect_to action: "index"
   end
 
+  def edit
+    redirect_to action: "index"
+  end
+
+  def update
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to action: "index"
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :body, :image)
+  end
 
   def move_to_index
     redirect_to action: "index" unless user_signed_in?
   end
 
-  private
-  def blog_params
-    binding.pry
-    params.permit(:title, :body)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'blogs#index'
-  resources :blogs , only:[:index,:new,:create]
-
+  resources :blogs
 end
